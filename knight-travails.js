563e96d3890c05b9b9c2f0713537ceb18d70ef9e
Mohammed Shafeek <mail2shafeek.sa@gmai.com>
2023-12-16 17:11:14 +0400

    Refined the code

--- knight-travails.js
+++ knight-travails.js
@@ -1,63 +1,21 @@
 class Cell{
-    constructor(row, col , parentCell = null) {
-        this.row = row;
-        this.col = col;
-        this.knightConnections = [];
+    constructor(cellIndex) {
+        this.index = cellIndex;
+        this.knightConnections = setKnightConnections(cellIndex[0],cellIndex[1]);
     }
 }
 
-const chessBoard = (function () {
-    let chessArray = [];
-    
-    for (i = 0; i < 8; i++) {
-        let rowArray = [];
-        for (j = 0; j < 8; j++) {
-            const cell = new Cell(i, j);
-            rowArray.push(cell);
-        }
-        chessArray.push(rowArray);
-    }
-    for (i = 0; i < 8; i++) {
-        for (j = 0; j < 8; j++) {
-            setConnections(chessArray[i][j],chessArray);
-        }
-    }
-    
-    function getCell(index) {
-        let row = index[0];
-        let col = index[1];
-        return chessArray[row][col];
-    }
 
-    console.log(chessArray);
-
-    return{getCell}
-
-})();
-
-function setConnections(cell, ipArray ,opArray = []) {
-    let row = cell.row;
-    let col = cell.col;
-    for (let i = 0; i < 8; i++){
-        for (let j = 0; j < 8; j++){
-            if (checkCombination(i, j, row, col) === true) {
-                opArray.push(ipArray[i][j]);
-            }
-        }   
-    }
-    cell.knightConnections = opArray;
+function setKnightConnections(row,col,opArray = []) {
+    const knightMoveOffsets = [[1, 2], [1, -2], [-1, 2], [-1, -2], [2, 1],[2, -1], [-2, 1],[-2, -1]];
+    knightMoveOffsets.forEach(offset => {
+        let index = [offset[0] + row, offset[1] + col];
+        if((offset[0] + row)<8 && (offset[0] + row)>=0 &&  (offset[1] + col)<8 && (offset[1] + col)>=0)
+            opArray.push(index);
+    });
+    return opArray;
 }
 
-function checkCombination(i, j, row, col) {
-    let rowDifference = Math.abs(i - row);
-    let colDifference = Math.abs(j - col);
-    if (rowDifference != colDifference && rowDifference + colDifference === 3 &&
-        Math.abs(rowDifference - colDifference) === 1) {
-        return true;
-    }
-    else
-        return false;
-}
 
 class Node{
     constructor(cell, path) {
@@ -84,38 +42,37 @@ const queue = (function () {
 })();
 
 function knightMoves(start, end) {
-    const startCell = chessBoard.getCell(start);
-    const endCell = chessBoard.getCell(end);
+    const startCell = new Cell(start);
+    const endCell = new Cell(end);
     const pathWay = findPath(startCell, endCell);
-    let opString = `You made it in ${pathWay.length} moves! Here is your path:\n`;
+    let opString = `You made it in ${pathWay.length-1} moves! Here is your path:\n`;
     pathWay.forEach(cell => {
-        opString += `[${cell.row},${cell.col}]\n`;
+        opString += `[${cell}]\n`;
     })
     console.log(opString);
 }
 
 function findPath(start, end) {
     let path = [];
-    path.push(start);
+    path.push(start.index);
     let node = new Node(start, path);
     queue.push(node);
     while (queue.size() > 0) {
         let currentNode = queue.pop();
-        if (currentNode.cell === end) {
-            console.log("Found");
-            console.log(currentNode.path);  
+        if ((currentNode.cell.index[0] === end.index[0])&&(currentNode.cell.index[1] === end.index[1])) {
             return currentNode.path;
         }
         else {
             let newPath =  [...currentNode.path];
-            currentNode.cell.knightConnections.forEach(cell => {
+            currentNode.cell.knightConnections.forEach(cellIndex => {
                 let individualPath = [...newPath];
-                individualPath.push(cell);
-                let newNode = new Node(cell, individualPath);
+                individualPath.push(cellIndex);
+                let newCell = new Cell(cellIndex);
+                let newNode = new Node(newCell, individualPath);
                 queue.push(newNode);
             });
         }
     }
 }
 
-knightMoves([3,3], [4,3]);
+knightMoves([0,0], [5,4]);
